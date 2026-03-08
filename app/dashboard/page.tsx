bfe2f0cfe9004e6711c786b740fd73b6a77769eb
loboconnect <gpinheirotrader@gmail.com>
2026-03-08 01:06:10 +0000

    fix dashboard use client first

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,6 +1,5 @@
-
-export const dynamic = "force-dynamic"
 "use client";
+export const dynamic = "force-dynamic";
 import { useState, useEffect } from "react";
 import { createBrowserClient } from "@supabase/ssr";
 import { usePushNotifications } from "@/components/PushNotificationProvider";
