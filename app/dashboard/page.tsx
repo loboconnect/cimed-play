aed4e4bc2e2ad91dddfa940204b7e629c85288a5
loboconnect <gpinheirotrader@gmail.com>
2026-03-08 01:03:16 +0000

    fix use client order

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,5 +1,5 @@
-export const dynamic = "force-dynamic"
 
+export const dynamic = "force-dynamic"
 "use client";
 import { useState, useEffect } from "react";
 import { createBrowserClient } from "@supabase/ssr";
